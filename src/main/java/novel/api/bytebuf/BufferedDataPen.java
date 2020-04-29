d96a7df6296f2c8de9a81c454b702f5b8afced78
umbreon22 <msw469@gmail.com>
2020-04-29 01:14:15 -0700

    Updated import to comply with novel refactor

--- src/main/java/novel/api/bytebuf/BufferedDataPen.java
+++ src/main/java/novel/api/bytebuf/BufferedDataPen.java
@@ -1,7 +1,7 @@
 package novel.api.bytebuf;
 
 
-import novel.api.types.write.DataPen;
+import novel.api.types.write.pens.DataPen;
 
 public interface BufferedDataPen<P extends BufferedDataPen<P>> extends Buffered, DataPen<P> {
 
