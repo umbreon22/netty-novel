38bea690fa998b18c6037ae451d3b4e78c8a2d0e
umbreon22 <sweis.m01@mymail.sxu.edu>
2020-09-02 03:03:29 -0500

    Added validator support to BufferedDataPaper

--- src/main/java/novel/api/bytebuf/BufferedDataPaper.java
+++ src/main/java/novel/api/bytebuf/BufferedDataPaper.java
@@ -1,7 +1,8 @@
 package novel.api.bytebuf;
 
 import novel.api.types.read.DataPaper;
+import novel.api.types.read.ProofreadingPaper;
 
-public interface BufferedDataPaper extends Buffered, DataPaper {
+public interface BufferedDataPaper extends Buffered, DataPaper, ProofreadingPaper {
 
 }
